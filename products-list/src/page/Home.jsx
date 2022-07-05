b3ffe0e6905bfd09fdbd8dd084444ca72724154e
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-07-05 21:48:17 +0400

    API foundation laid

--- products-list/src/page/Home.jsx
+++ products-list/src/page/Home.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
+const getProducts = async () => {
+  const res = await fetch("https://dummyjson.com/products");
+  return res.json();
+};
+
 const Home = () => {
+  const { data, status } = useQuery("planets", getProducts);
+  console.log(data);
+
   return (
     <>
       <Navbar />
